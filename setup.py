ad63bb9511d02df9728fd9d3d9fa186de5fd14d9
Danc3200 <danielc3200@gmail.com>
2023-02-07 15:44:14 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from glob import glob
+import os
+
+package_name = 'bazu'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.py')),
+        (os.path.join('share', package_name), glob('urdf/*')),
+        (os.path.join('share', package_name), glob('meshes/*.stl')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='daniel',
+    maintainer_email='daniel@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        'trajectory_generator = bazu.trajectory_gen:main',
+        'trajectory_generator_2 = bazu.trajectory_gen_2pos:main',
+        ],
+    },
+)
